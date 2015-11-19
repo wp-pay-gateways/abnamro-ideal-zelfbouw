c237bcb5215dcad47204e6fa575717c98349a8f6
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 08:38:22 +0100

    Extend from abstract iDEAL Advanced integration class.

--- src/Integration.php
+++ src/Integration.php
@@ -1,22 +1,10 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_AbnAmro_IDealZelfbouw_Integration {
+class Pronamic_WP_Pay_Gateways_AbnAmro_IDealZelfbouw_Integration extends Pronamic_WP_Pay_Gateways_IDealAdvanced_AbstractIntegration {
 	public function __construct() {
 		$this->id         = 'abnamro-ideal-zelfbouw';
 		$this->name       = 'ABN AMRO - iDEAL Zelfbouw';
 		$this->provider   = 'abnamro';
 		$this->deprecated = true;
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvanced_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvanced_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvanced_Gateway';
 	}
 }
